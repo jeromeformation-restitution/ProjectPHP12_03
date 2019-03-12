930b236faf3e5974064a68d0eebf4abf6ed31e32
jerome <ambroise.jerome.formation@gmail.com>
2019-03-12 11:51:48 +0100

    Création de la request et la response et aussi l'envoi au navigateur avec la fonction send

--- public/index.php
+++ public/index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Bonjour les enfants";
+
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+
+require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+$request = ServerRequest::fromGlobals();
+$response = new Response (200, [],'coucou');
+
+
+\Http\Response\send($response);
